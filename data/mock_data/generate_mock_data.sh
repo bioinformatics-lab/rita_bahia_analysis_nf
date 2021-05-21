4534151f6ba8d8011139850243c9f4ad7431454c
Davi Marcon <48180517+Mxrcon@users.noreply.github.com>
2021-05-21 16:46:52 -0300

    updating pipeline following camila_sao_paulo `d62efe0f03faec81d7ece07973b608af988cfadd` (#7)

--- data/mock_data/generate_mock_data.sh
+++ data/mock_data/generate_mock_data.sh
@@ -1,5 +1,10 @@
 set -uex
 
-
-touch GenomeAnalysisTK.jar
-
+touch data/mock_data/GenomeAnalysisTK.jar
+touch data/mock_data/ref01.gbk
+touch data/mock_data/001_R1.fastq.gz
+touch data/mock_data/001_R2.fastq.gz
+touch data/mock_data/002_R1.fastq.gz
+touch data/mock_data/002_R2.fastq.gz
+touch data/mock_data/003_R1.fastq.gz
+touch data/mock_data/003_R2.fastq.gz
